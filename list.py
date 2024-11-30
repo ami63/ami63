d5f6373ab35e0f2cf41b6468bca905ccba8666fc
Amit Rai <amitrai.india16@gmail.com>
2024-11-30 09:41:37 -0800

    list and tuple

---
+++ list.py
@@ -0,0 +1,10 @@
+movies = []
+mov1= input("enter the first movie: ")
+mov2= input("enter the second movie: ")
+mov3= input("enter the third movie: ")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
